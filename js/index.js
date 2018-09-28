d46b7cfccc791b6c4987c0d430109e3a668bf608
Marco Stevanon <marco.stevanon@gmail.com>
2018-09-28 16:19:32 +0200

    add course list creation

--- js/index.js
+++ js/index.js
@@ -18,15 +18,23 @@ var requests = []
 async function load() {
     var courseList = await fetch(`${base_url}course/list`).then(res => res.json());
     console.log(courseList);
+    
+    //List creation
     courseList.forEach(item => {
-        $('#course-list').append(item);
+        $('#course-list').append(`<li class="nav-item">
+            <a class="nav-link" href="?q=${item.csvCode}">
+                <button class="btn btn-sm btn-block 
+                    ${item.csvCode === query ? 'btn-light' : 'btn-outline-light'} 
+                    btn-bold" type="button">${item.name}</button>
+            </a></li>`);
     });
-
 
     if (query) {
         var courseCurrent = await fetch(`${base_url}course/${query}`).then(res => res.json());
         var lastUpdate = await fetch(`${base_url}lastupdt/${query}`).then(res => res.json());
         var times = await fetch(`${base_url}times/${query}?date=2018-09-20`).then(res => res.json());
+
+        $('#title').text(`${courseCurrent.type} - ${courseCurrent.name}`);
 
 
 
