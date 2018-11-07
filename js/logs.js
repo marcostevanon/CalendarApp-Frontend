234c62ac36b90a67be224336f75631467e382133
Marco Stevanon <marco.stevanon@gmail.com>
2018-11-07 08:18:44 +0100

    add mail sending log page

--- js/logs.js
+++ js/logs.js
@@ -2,13 +2,27 @@ fetch(`https://marcostevanon.ovh:1883/log`)
     .then(res => res.json())
     .then(res => {
         res.forEach(item => {
-            $('#table-entry').append(
+            $('#scraper-table').append(
                 `<tr>
                     <th scope="row">${item.id}</th>
                     <td>${moment(item.date).format('DD/MM/YYYY H:mm:ss')}</td>
                     <td>${item.courseCode}</td>
                     <td>${item.action}</td>
                     <td>${item.prof}</td>
+                    <td>${item.times}</td>
+                </tr>`);
+        });
+    });
+
+fetch(`https://marcostevanon.ovh:1883/mail`)
+    .then(res => res.json())
+    .then(res => {
+        res.forEach(item => {
+            $('#scraper-table').append(
+                `<tr>
+                    <th scope="row">${item.id}</th>
+                    <td>${moment(item.date).format('DD/MM/YYYY H:mm:ss')}</td>
+                    <td>${item.action}</td>
                     <td>${item.times}</td>
                 </tr>`);
         });
